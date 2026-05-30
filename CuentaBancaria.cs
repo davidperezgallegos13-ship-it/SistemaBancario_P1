08b32a4b70ee0f46405a4e4a25e1085c783c11a1
ASUS Vivobook <ASUS Vivobook@DESKTOP-HCS047R>
2026-05-29 22:04:02 -0500

    Agregar archivos de proyecto.

---
+++ CuentaBancaria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaBancario_P1
+{
+    public class CuentaBancaria
+    {
+        public string NumeroCuenta { get; set; }
+        public decimal Saldo { get; private set; }
+
+        public CuentaBancaria(string numeroCuenta, decimal saldoInicial)
+        {
+            NumeroCuenta = numeroCuenta;
+            Saldo = saldoInicial;
+        }
+
+        public void Depositar(decimal monto)
+        {
+            if (monto <= 0) throw new ArgumentException("El monto debe ser mayor a cero");
+            Saldo += monto;
+        }
+
+        public void Retirar(decimal monto)
+        {
+            if (monto <= 0) throw new ArgumentException("El monto debe ser mayor a cero");
+            if (monto > Saldo) throw new InvalidOperationException("Fondos insuficientes");
+            Saldo -= monto;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario_P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Sistema Bancario .NET 4.7.2 Iniciado ---");
+
+            // Instanciamos el ejemplo para que el ejecutable sea válido
+            CuentaBancaria cuenta = new CuentaBancaria("123456", 100);
+            cuenta.Depositar(50);
+
+            Console.WriteLine("Prueba de consola exitosa. Saldo actual: " + cuenta.Saldo);
+            Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
